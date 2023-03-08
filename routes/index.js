948f69898f5fdaccea81e04f2b65b84c899e90a8
PunnawatPinsaeng <s6406021620050@email.kmutnb.ac.th>
2023-03-08 11:18:07 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Base URL for the API
 // const base_url = "https://api.example.com";
-const base_url = "http://localhost:5500";
+const base_url = "http://node42074-punnawat.proen.app.ruk-com.cloud:11354";
 
 // Set the template engine
 app.set('views' , path.join(__dirname , '/public/views'));
